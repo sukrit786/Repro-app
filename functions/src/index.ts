ec8b422fef01dc4466f08674cfa16e63c8b215d7
sukrit0786 <sukritanand57@gmail.com>
2020-01-03 21:54:58 +0530

    still errors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 import * as functions from 'firebase-functions';
-const { app } = require(`${process.cwd()}/dist/server/main.js`);
+const { app } = require(`${process.cwd()}/dist/server`);
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
